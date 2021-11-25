542461fc37b789f3b83a5ce5600ea5ea4ad80aa2
Rogelio Navarrete <75106311+RogelioMdz@users.noreply.github.com>
2021-11-25 16:21:25 -0600

    Update config.php

--- config/config.php
+++ config/config.php
@@ -2,11 +2,11 @@
 /**
  *  Este es el link que se debe cambiar al querer cambiar de host y/o servidor
  */
-define('URL', 'http://localhost:8888/Sistema-automatizador-restaurante/');
-define('DASH', 'http://localhost:8888/Sistema-automatizador-restaurante/dashboard');
-define('MESAS', 'http://localhost:8888/Sistema-automatizador-restaurante/mesasdisponibles');
-define('HAMBURGUESAS', 'http://localhost:8888/Sistema-automatizador-restaurante/hamburguesas');
-define('NUEVAORDEN', 'http://localhost:8888/Sistema-automatizador-restaurante/nuevaorden');
+define('URL', 'http://localhost:8080/Sistema-automatizador-restaurante/');
+define('DASH', 'http://localhost:8080/Sistema-automatizador-restaurante/dashboard');
+define('MESAS', 'http://localhost:8080/Sistema-automatizador-restaurante/mesasdisponibles');
+define('HAMBURGUESAS', 'http://localhost:8080/Sistema-automatizador-restaurante/hamburguesas');
+define('NUEVAORDEN', 'http://localhost:8080/Sistema-automatizador-restaurante/nuevaorden');
 /**
  *  declaro las constantes para posterioremente conectarme a la base de datos en el archivo database.php
  */
